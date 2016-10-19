ae12146049be9a3d98329646032ea9d4f813778d
Tristan Ludowyk <tristan@jtribe.com.au>
2016-10-19 15:14:56 +1100

    Fixing weird - character

--- components/setup.sh
+++ components/setup.sh
@@ -17,7 +17,7 @@ function setup() {
 
   comp_deinit
 
-  bundle exec pod check || bundle exec pod install $verboseArg || bundle exec pod install —-repo-update $verboseArg
+  bundle exec pod check || bundle exec pod install $verboseArg || bundle exec pod install --repo-update $verboseArg
 
   if [[ -f Cartfile ]]; then
     if [[ -f Carthage/Build.tar.gz ]]; then
