f07b6c09049a35e55ec48e2e7d78e9ed9aca55d3
Tristan Ludowyk <tristan@jtribe.com.au>
2016-10-28 13:42:36 +1100

    Comments

--- components/setup.sh
+++ components/setup.sh
@@ -8,7 +8,8 @@ function setup() {
   # This is the bundle command that circle uses 
   bundle check || bundle install --jobs 4 --retry 3
 
-  # Fix for circle error code 65?
+  # pod check doesn't seem to know when the Pods cache is out of date on CircleCI, 
+  # so force an install if a pod was updated by a developer
   diff Podfile.lock Pods/Manifest.lock > /dev/null || bundle exec pod install
 
   # pod install may take 25 mins on circle if it has to download the master spec repo             
